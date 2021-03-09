3e1954fb8018967c8bdd9c9420845e6d6d6d9c38
MingxueZhang <38911694+Col1nMuLLer@users.noreply.github.com>
2021-03-09 23:52:35 +0800

    Update question3.scala

--- Question 3/question3.scala
+++ Question 3/question3.scala
@@ -1,3 +1,3 @@
-Some problems encourted when I want to use tensorflow.
+Some problems encountered when I want to use TensorFlow.
 I'm working in scala. Even though it may late than the deadline, I still want to try.
 Please ignore this.
